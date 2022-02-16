2363a834db5097257f2dd13555b0083dd6a795bb
korolev-rostislav <99824640+korolev-rostislav@users.noreply.github.com>
2022-02-16 20:40:31 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>TEST</title>
+</head>
+<body>
+   
+	<ul>1</ul> 
+    <ul>2</ul> 
+    <ul>3</ul>
+
+</body>
+</html>
